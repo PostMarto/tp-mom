8f501c97b725f853bc1b40da3c143abeb2afe367
Vitiligio <mpalazon3@gmail.com>
2026-09-17 03:04:25 -0300

    entrega

--- golang/go.mod
+++ golang/go.mod
@@ -1,4 +1,4 @@
-module "github.com/7574-sistemas-distribuidos/tp-mom/golang"
+module github.com/7574-sistemas-distribuidos/tp-mom/golang
 
 go 1.24.1
 
@@ -10,5 +10,6 @@ require (
 require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	//github.com/rabbitmq/rabbitmq-amqp-go-client v1.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- golang/internal/factory/factory.go
+++ golang/internal/factory/factory.go
@@ -1,13 +1,22 @@
 package factory
 
 import (
+	"github.com/7574-sistemas-distribuidos/tp-mom/golang/internal/factory/implementations"
 	m "github.com/7574-sistemas-distribuidos/tp-mom/golang/internal/middleware"
 )
 
 func CreateQueueMiddleware(queueName string, connectionSettings m.ConnSettings) (m.Middleware, error) {
-	return nil, nil
+	factory, err := implementations.NewQueueFactory(queueName, connectionSettings)
+	if err != nil {
+		return nil, err
+	}
+	return &factory, nil
 }
 
 func CreateExchangeMiddleware(exchange string, keys []string, connectionSettings m.ConnSettings) (m.Middleware, error) {
-	return nil, nil
+	factory, err := implementations.NewExchangeFactory(exchange, keys, connectionSettings)
+	if err != nil {
+		return nil, err
+	}
+	return &factory, nil
 }
